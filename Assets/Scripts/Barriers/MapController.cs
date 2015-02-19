ce7e5b23a66793e949ed44e26a3e48d3a925a97f
hellraptor <hellraptr@gmail.com>
2015-02-20 00:27:10 +0300

    Experiments with map representation
    Modefite robot wiew
    mod barrier wiew
    added map wiew

--- Assets/Scripts/Barriers/MapController.cs
+++ Assets/Scripts/Barriers/MapController.cs
@@ -1,20 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using AssemblyCSharp;
 
 public class MapController : MonoBehaviour
 {
-
-
-		public Transform barrier;
+		public Transform someBarrier;
 		public int  barrierCount = 10;
 		public Vector2 mapSize = new Vector2 (20, 20);
 		// Use this for initialization
 		void Start ()
 		{
 				for (int i=0; i<barrierCount; i++) {
-						Instantiate (barrier, new Vector3 (i * 2.0F, 1, 0), Quaternion.identity);
+						Instantiate (someBarrier, new Vector3 (Random.Range (-mapSize.x, mapSize.x), 0.5f, Random.Range (-mapSize.y, mapSize.y)), Quaternion.identity);
 				}
-	
 		}
-
 }
