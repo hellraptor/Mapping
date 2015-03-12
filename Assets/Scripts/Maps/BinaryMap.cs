14451d476b8a17525032d73de77a4f2b3ae0c51a
hellraptor <hellraptr@gmail.com>
2015-03-12 12:20:31 +0300

    BianryMap added

---
+++ Assets/Scripts/Maps/BinaryMap.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BinaryMap : MonoBehaviour {
+
+    public RectTransform cell;
+    public RectTransform mapPanel;
+	// Use this for initialization
+	void Start () {
+
+        
+        for (int i=0; i<10; i++)
+        {
+            ((RectTransform)Instantiate(cell,  new Vector3(i * 2f, 0, 0) , Quaternion.identity)).SetParent(this.transform);            
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
