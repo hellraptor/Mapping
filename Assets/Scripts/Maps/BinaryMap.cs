d543d70366ff01e39705a6f0920df339ec3b3d83
hellraptor <hellraptr@gmail.com>
2015-03-13 12:01:36 +0300

    added process robot collision with barriers

--- Assets/Scripts/Maps/BinaryMap.cs
+++ Assets/Scripts/Maps/BinaryMap.cs
@@ -6,14 +6,11 @@ public class BinaryMap : MonoBehaviour {
     public RectTransform cell;
     public RectTransform mapPanel;
 	// Use this for initialization
-	void Start () {
-
-        
+	void Start () {        
         for (int i=0; i<10; i++)
         {
-            ((RectTransform)Instantiate(cell,  new Vector3(i * 2f, 0, 0) , Quaternion.identity)).SetParent(this.transform);            
-        }
-	
+            ((RectTransform)Instantiate(cell,  new Vector3(i * 2f, 50, 0) , Quaternion.identity)).SetParent(this.transform);            
+        }	
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/Robot/RobotController.cs
+++ Assets/Scripts/Robot/RobotController.cs
@@ -1,56 +1,75 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
 
 public class RobotController : MonoBehaviour
 {
-		public GUIText textInfo;
-		float rotationAngle = 0;
-		public float rotationSpeed = 1;
-		public float moveSpeed = 1;
-		public Vector3 position;
+    public GUIText textInfo;
+    float rotationAngle = 0;
+    public float rotationSpeed = 1;
+    public float moveSpeed = 1;
+    public Vector3 position;
+    public Transform map;
+    public Transform point;
+    string collisionCount = "";
+    HashSet<ContactPoint> pointsCloud;
 
-		void Start ()
-		{
-				textInfo.text = "Velcome. Info about will bee heare:";
-				position = new Vector3 (0, 0.5f, 0);
+    void Start()
+    {
+        textInfo.text = "Velcome. Info about will bee heare:";
+        position = new Vector3(0, 0.5f, 0);
+        pointsCloud = new HashSet<ContactPoint>();
 
-		}
+    }
 
-		void FixedUpdate ()
-		{
+    void FixedUpdate()
+    {
+        UpdateRobotDirection();
+        UpdateRobotPosition();
+        UpdateGameTextInfo(rotationAngle, 0f);                  
+    }
 
-				UpdateRobotDirection ();
-				UpdateRobotPosition ();
-				UpdateGameTextInfo (rotationAngle, 0f);
-						
-		}
+    void UpdateRobotDirection()
+    {
+        if (rotationAngle > 360 || rotationAngle < -360)
+            rotationAngle = 0;
+        rotationAngle += Input.GetAxisRaw("Horizontal") * rotationSpeed;
+        Quaternion target = Quaternion.Euler(0, rotationAngle, 90);
+        transform.rotation = target;
+    }
 
-		void UpdateRobotDirection ()
-		{
-				if (rotationAngle > 360 || rotationAngle < -360)
-						rotationAngle = 0;
-				rotationAngle += Input.GetAxisRaw ("Horizontal") * rotationSpeed;
-				Quaternion target = Quaternion.Euler (0, rotationAngle, 90);
-				transform.rotation = target;
-		}
+    /**
+     * 
+     * calls on every collision robot with somthing
+     * and writes coordinates of the collision in the 
+     * pointsCloud {@code HashSet}
+    */
+    void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts [0];   
+        pointsCloud.Add(contact);
+        collisionCount = pointsCloud.Count().ToString();
+    }
 
-		void UpdateRobotPosition ()
-		{
-				float dZ = Mathf.Sin (Mathf.Deg2Rad * rotationAngle) * moveSpeed * Input.GetAxisRaw ("Vertical");
-				float dX = Mathf.Cos (Mathf.Deg2Rad * rotationAngle) * moveSpeed * Input.GetAxisRaw ("Vertical");
-				position = new Vector3 (position.x + dZ, 0.5f, position.z + dX);
-				transform.position = position;
-		}
+    void UpdateRobotPosition()
+    {
+        float dZ = Mathf.Sin(Mathf.Deg2Rad * rotationAngle) * moveSpeed * Input.GetAxisRaw("Vertical");
+        float dX = Mathf.Cos(Mathf.Deg2Rad * rotationAngle) * moveSpeed * Input.GetAxisRaw("Vertical");
+        position = new Vector3(position.x + dZ, 0.5f, position.z + dX);
+        transform.position = position;
+    }
 
-		void UpdateGameTextInfo (float angle, float offset)
-		{
-				textInfo.text = "";
-				textInfo.text += "\nRotation Angle: " + angle.ToString ();				
-				textInfo.text += "\nPosition: " + position.ToString ();	
-				textInfo.text += "\nOffset: " + offset.ToString ();	
-				textInfo.text += "\nX: " + transform.position.x;	
-				textInfo.text += "\nZ: " + transform.position.z;	
+    void UpdateGameTextInfo(float angle, float offset)
+    {
+        textInfo.text = "";
+        textInfo.text += "\nRotation Angle: " + angle.ToString();               
+        textInfo.text += "\nPosition: " + position.ToString();  
+        textInfo.text += "\nOffset: " + offset.ToString();  
+        textInfo.text += "\nX: " + transform.position.x;    
+        textInfo.text += "\nZ: " + transform.position.z;  
+        textInfo.text += "\nCollision point: " + collisionCount;  
 
-		}
+    }
 }
 
