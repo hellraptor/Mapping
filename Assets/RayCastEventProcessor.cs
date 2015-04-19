32e0ed0930fcf7ebd12a1cc4cc6716032134a9ae
hellraptor <hellraptr@gmail.com>
2015-04-19 20:44:37 +0300

    scaner added

---
+++ Assets/RayCastEventProcessor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCastEventProcessor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("1111");
+    }
+}
---
+++ Assets/Scripts/Robot/Sensors/Scaner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scaner : MonoBehaviour
+{
+
+    public float viewingAngle;
+    public float resolution;
+    int rayCount;
+    public float detectionDistance;
+    public Transform rayPattern;
+
+    // Use this for initialization
+    void Start()
+    {
+        rayCount = (int)(viewingAngle / resolution);
+        //for (int i = 0; i < rayCount; i++)
+        //{
+        //    float rayDirection = (-viewingAngle * 0.5f) + (i * resolution);
+        //    Transform ray = (Transform)Instantiate(rayPattern, transform.position, Quaternion.Euler(rayDirection, 0f, 0f));
+        //    ray.SetParent(transform);
+        //    ray.localScale = Vector3.one;
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, fwd, out hit, 10))
+        {
+            Debug.DrawLine(transform.position, hit.point);
+        }
+    }
+}
+
+
+
+
